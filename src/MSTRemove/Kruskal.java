8cc12ec2ffabc8554c8999e66b348b078c5b6fb5
Tulebaeva Marzhan <241398@astanait.edu.kz>
2025-11-13 22:52:17 +0500

    Kruskal

--- src/MSTRemove/Kruskal.java
+++ src/MSTRemove/Kruskal.java
@@ -1,4 +1,20 @@
 package MSTRemove;
+import java.util.*;
 
 public class Kruskal {
+    public static List<Edge> buildMST(Graph g) {
+        List<Edge> sortedEdges = new ArrayList<>(g.edges);
+        Collections.sort(sortedEdges);
+
+        UnionFind uf = new UnionFind(g.V);
+        List<Edge> mst = new ArrayList<>();
+
+        for (Edge e : sortedEdges) {
+            if (uf.union(e.u, e.v)) {
+                mst.add(e);
+            }
+        }
+        return mst;
+    }
 }
+
