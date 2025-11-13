70a91752261a67b674f5706db09dd0ec8f223f33
Tulebaeva Marzhan <241398@astanait.edu.kz>
2025-11-13 23:04:33 +0500

    added main workflow

--- src/MSTRemove/Main.java
+++ src/MSTRemove/Main.java
@@ -1,17 +1,48 @@
 package MSTRemove;
+import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Graph g = new Graph(7);
+        // sample graph
+        g.addEdge(0, 1, 7);
+        g.addEdge(0, 3, 5);
+        g.addEdge(1, 2, 8);
+        g.addEdge(1, 3, 9);
+        g.addEdge(1, 4, 7);
+        g.addEdge(2, 4, 5);
+        g.addEdge(3, 4, 15);
+        g.addEdge(3, 5, 6);
+        g.addEdge(4, 5, 8);
+        g.addEdge(4, 6, 9);
+        g.addEdge(5, 6, 11);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Building MST...");
+        List<Edge> mst = Kruskal.buildMST(g);
+
+        System.out.println("\nMST edges:");
+        for (Edge e : mst) System.out.println(e);
+
+        // choose an edge to remove
+        Edge removed = mst.get(2);
+        mst.remove(removed);
+
+        System.out.println("\nRemoved edge: " + removed);
+
+        // components after removal
+        List<List<Integer>> comps = g.getComponents(mst, removed);
+        System.out.println("\nComponents:");
+        System.out.println(" - " + comps.get(0));
+        System.out.println(" - " + comps.get(1));
+
+        // find replacement
+        Edge repl = g.findReplacement(comps);
+        System.out.println("\nReplacement edge: " + repl);
+
+        // rebuild MST
+        mst.add(repl);
+
+        System.out.println("\nNew MST:");
+        for (Edge e : mst) System.out.println(e);
     }
 }
