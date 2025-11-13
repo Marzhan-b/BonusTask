711d1b616bebf24e692452048322c30b8cf71799
Tulebaeva Marzhan <241398@astanait.edu.kz>
2025-11-13 22:26:49 +0500

    add Union Find for Kruskal

--- src/MSTRemove/UnionFind.java
+++ src/MSTRemove/UnionFind.java
@@ -1,4 +1,31 @@
 package MSTRemove;
 
 public class UnionFind {
+    int[] parent,rank;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        rank = new int[n];
+
+        for (int i = 0; i < n; i++) parent[i] = i;
+    }
+    public int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    public boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY) return false;
+
+        if (rank[rootX] < rank[rootY]) parent [rootX] = rootY;
+        else if (rank[rootY] < rank[rootX]) parent [rootY] = rootX;
+        else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+        return true;
+    }
 }
