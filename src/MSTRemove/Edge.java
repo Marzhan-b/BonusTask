3be84f4de97141056781279106af2fa60e8b0c24
Tulebaeva Marzhan <241398@astanait.edu.kz>
2025-11-13 22:15:32 +0500

    added Edge.java with basic edge structure and compareTo implementation

--- src/MSTRemove/Edge.java
+++ src/MSTRemove/Edge.java
@@ -1,4 +1,20 @@
 package MSTRemove;
+public class Edge implements Comparable<Edge> {
+    public int u, v, weight;
 
-public class Edge {
+    public Edge(int u, int v, int weight) {
+        this.u = u;
+        this.v = v;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return Integer.compare(this.weight, other.weight);
+    }
+
+    @Override
+    public String toString() {
+        return u + " - " + v + " (" + weight + ")";
+    }
 }
